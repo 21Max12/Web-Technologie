46ca84e88ffb1892e1e26a20581317643d283f53
Max <herzog_maximilian@gmx.net>
2023-11-11 17:14:06 +0100

    Update main.py

--- env/main.py
+++ env/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -16,8 +16,15 @@ class Message(db.Model):
 
 
 
-@app.route("/")
-def start_page():
+@app.route("/<name>", methods=['GET', 'POST'])
+def start_page(name):
+    if request.method =='POST':
+        new_message = Message(
+            user = name,
+            content = request.form['content']
+        )
+        db.session.add(new_message)
+        db.session.commit()
     return render_template('index.html')
 
 if __name__ == "__main__":
